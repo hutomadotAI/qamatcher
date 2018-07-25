2acab9357fd902880b5f4d40a772e2fc139e6e0f
Paul Annetts <paul.annetts@hutoma.com>
2018-07-25 15:23:36 +0000

    Merged PR 3132: Make sure report training progress back to API every 10s or so
    - Add training progress callbacks
    - Make sure report back to API every 10s or so
    That will ensure API will not timeout the training and reassign it.
    
    Tested with huge file on k8s proto environment.
    
    Cherry picked from !3131
    
    Related work items: #5570

--- src/embedding/training_process.py
+++ src/embedding/training_process.py
@@ -32,6 +32,9 @@ class TrainEmbedMessage(aitp.TrainingMessage):
         super().__init__(ai_path, ai_id, max_training_mins)
 
 
+UPDATE_EVERY_N_SECONDS = 10.0
+
+
 class EmbedTrainingProcessWorker(aitp.TrainingProcessWorkerABC):
     def __init__(self, pool, asyncio_loop, aiohttp_client_session=None):
         super().__init__(pool, asyncio_loop)
@@ -42,9 +45,12 @@ class EmbedTrainingProcessWorker(aitp.TrainingProcessWorkerABC):
 
         self.aiohttp_client = aiohttp_client_session
         config = SvcConfig.get_instance()
-        self.w2v_client = Word2VecClient(
-            config.w2v_server_url, self.aiohttp_client)
-        self.entity_wrapper = EntityWrapper(config.er_server_url, self.aiohttp_client)
+        self.w2v_client = Word2VecClient(config.w2v_server_url,
+                                         self.aiohttp_client)
+        self.entity_wrapper = EntityWrapper(config.er_server_url,
+                                            self.aiohttp_client)
+        self.last_update_sent = None
+        self.callback = None
 
     async def get_vectors(self, questions):
         word_dict = {}
@@ -54,7 +60,23 @@ class EmbedTrainingProcessWorker(aitp.TrainingProcessWorkerABC):
                 word_dict[word] = ""
         return await self.w2v_client.get_vectors_for_words(word_dict)
 
-    async def train(self, msg, topic: ait.Topic, callback_object):
+    def report_progress(self, progress_value):
+        now = datetime.datetime.utcnow()
+        seconds_since_update = (now - self.last_update_sent).total_seconds() \
+            if self.last_update_sent is not None \
+            else (UPDATE_EVERY_N_SECONDS + 1.0)
+        if seconds_since_update > UPDATE_EVERY_N_SECONDS:
+            if self.callback is not None:
+                self.callback.report_progress(progress_value)
+                self.callback.check_for_cancel()
+            self.last_update_sent = now
+
+    async def train(self, msg, topic: ait.Topic, callback):
+        # handshake with API to say we're starting training
+        self.callback = callback
+        self.last_update_sent = None
+        if callback is not None:
+            await callback.wait_to_save()
 
         training_file_path = msg.ai_path / ait.AI_TRAINING_STANDARD_FILE_NAME
         self.logger.info(
@@ -72,27 +94,42 @@ class EmbedTrainingProcessWorker(aitp.TrainingProcessWorkerABC):
             temp_data_file = tempdir_path / DATA_FILE
 
             self.logger.info("Extracting entities...")
-            entities = [await self.entity_wrapper.extract_entities(s) for s in x]
+            entities = []
+            for question in x:
+                entity = await self.entity_wrapper.extract_entities(question)
+                entities.append(entity)
+                self.report_progress(0.1)
             self.entity_wrapper.save_data(temp_data_file, entities, y)
+            self.report_progress(0.2)
 
             self.logger.info(
                 "Entities saved to {}, tokenizing...".format(temp_data_file))
-            x_tokens = [await self.entity_wrapper.tokenize(s) for s in x]
-            self.logger.info("tokens: {}".format(
-                [(xx, toks) for xx, toks in zip(x, x_tokens)]))
+
+            x_tokens = []
+            for question in x:
+                tokens = await self.entity_wrapper.tokenize(question)
+                x_tokens.append(tokens)
+                self.logger.info("tokens: {}".format((question, tokens)))
+                self.report_progress(0.3)
+
             x_tokens_set = list(set([w for l in x_tokens for w in l]))
+            self.report_progress(0.4)
 
             words = {}
             for l in x_tokens_set:
                 words[l] = None
 
             vecs = await self.get_vectors(list(words.keys()))
+            self.report_progress(0.6)
 
             cls = EmbeddingComparison(w2v=vecs)
             self.logger.info("Fitting...")
             cls.fit(x_tokens, y)
+            self.report_progress(0.8)
+
             cls.save_model(temp_model_file)
             self.logger.info("Saved model to {}".format(temp_model_file))
+            self.report_progress(0.9)
 
             self.logger.info("Moving training files to {}".format(msg.ai_path))
             model_file = msg.ai_path / MODEL_FILE
