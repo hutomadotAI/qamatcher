b70bd682c1d2e79fcfd68fa1f19b58b2e33a726a
thomas <thomas@hutoma.com>
2018-07-26 16:52:06 +0100

    more test fixes

--- src/embedding/tests/test_embedding_training.py
+++ src/embedding/tests/test_embedding_training.py
@@ -90,7 +90,7 @@ async def test_er_entities(mocked_train):
     question = "this is a dummy question that will be mocked out"
     entities = await mocked_train.entity_wrapper.extract_entities(question)
     assert len(entities) == 2
-    assert entities[0]['value'] == 'reading'
+    assert entities[0]['value'] == 'Reading'
     assert entities[1]['value'] == 'today'
 
 
@@ -106,21 +106,21 @@ async def test_er_match_entities_none(mocked_train):
     question = "this question has no matching entities"
     matched_label = mocked_train.entity_wrapper.match_entities(
         question)
-    assert matched_label is None
+    assert len(matched_label) == 0
 
 
 async def test_er_match_entities_1(mocked_train):
     question = "this question matches Reading"
     matched_label = mocked_train.entity_wrapper.match_entities(
         question)
-    assert matched_label == "You said Reading today"
+    assert matched_label[0][1] == "You said Reading today"
 
 
 async def test_er_match_entities_2(mocked_train):
     question = "this question matches Bloggs Fred"
     matched_label = mocked_train.entity_wrapper.match_entities(
         question)
-    assert matched_label == "You said Paris Fred Bloggs"
+    assert matched_label[0][1] == "You said Paris Fred Bloggs"
 
 async def test_train_success(mocked_train, mocker):
     DUMMY_AIID = "123456"
