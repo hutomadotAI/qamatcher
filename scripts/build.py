050c0199a64af415fe27cd59c01e5295bb5aae27
Paul Annetts <paul@hutoma.ai>
2019-03-25 17:34:19 +0000

    Reformat build.py

--- scripts/build.py
+++ scripts/build.py
@@ -4,7 +4,6 @@ import argparse
 import os
 from pathlib import Path
 import subprocess
-import uuid
 
 import hu_build.build_docker
 from hu_build.build_docker import DockerImage
@@ -21,23 +20,31 @@ def main(build_args):
     src_path = ROOT_DIR / 'src'
 
     os.chdir(src_path)
-    subprocess.run(["docker", "build", ".", "-f", "Dockerfile.base", "-t", "emb_base"], check=True)
+    subprocess.run(
+        ["docker", "build", ".", "-f", "Dockerfile.base", "-t", "emb_base"],
+        check=True)
 
     docker_image = DockerImage(
-        src_path / ("Dockerfile."+component),
-        'backend/embedding-'+component,
+        src_path / ("Dockerfile." + component),
+        'backend/embedding-' + component,
         image_tag=tag_version,
         registry='eu.gcr.io/hutoma-backend')
     hu_build.build_docker.build_single_image(
-        "ai-embedding-"+component, docker_image, push=build_args.docker_push)
+        "ai-embedding-" + component, docker_image, push=build_args.docker_push)
     image_name = docker_image.full_image_tag()
     print("*** Building extracting results from images using container")
-    result = subprocess.run(["docker", "create", image_name], stdout=subprocess.PIPE,
-        encoding="utf8", check=True)
+    result = subprocess.run(["docker", "create", image_name],
+                            stdout=subprocess.PIPE,
+                            encoding="utf8",
+                            check=True)
     container_id = result.stdout.strip()
-    subprocess.run(["docker", "cp", "{}:/tmp/tests.xml".format(container_id), "TESTS-emb.xml"])
+    subprocess.run([
+        "docker", "cp", "{}:/tmp/tests.xml".format(container_id),
+        "TESTS-emb.xml"
+    ])
     print("*** Cleaning up")
     subprocess.run(["docker", "rm", "-v", "{}".format(container_id)])
+
 
 if __name__ == "__main__":
     PARSER = argparse.ArgumentParser(
